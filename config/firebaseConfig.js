a17e26456087a799f85e8f5b6a6a0fd8486f5774
Joshtri <stuffofyos1516@gmail.com>
2024-02-02 09:06:11 +0800

    lts

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -7,7 +7,13 @@ const { getAuth } = require("firebase/auth");
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-
+  apiKey: "AIzaSyAcFUZnpAU5qcL4BVPHgHuFEHb1Qw-HOTI",
+  authDomain: "node-app-litlane.firebaseapp.com",
+  projectId: "node-app-litlane",
+  storageBucket: "node-app-litlane.appspot.com",
+  messagingSenderId: "605950190792",
+  appId: "1:605950190792:web:fdb655b8addee1658bb1fd",
+  measurementId: "G-TC19BXGVJV"
 };
 
 const app = initializeApp(firebaseConfig);
