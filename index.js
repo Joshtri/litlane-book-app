d196c2a585aa1dad8f93c92d05480b32b110faff
Joshtri <stuffofyos1516@gmail.com>
2024-02-02 11:18:40 +0800

    lts

--- index.js
+++ index.js
@@ -47,10 +47,11 @@ app.use(
     saveUninitialized: true,
     cookie: {
       maxAge: 360000,  // Waktu kadaluarsa dalam milidetik (6 menit)
-      secure: false,   // Hanya mengirim cookie melalui HTTPS
+      secure: true,   // Hanya mengirim cookie melalui HTTPS
       sameSite: 'none', // Sesuaikan sesuai kebutuhan keamanan
       // Jika Anda menggunakan subdomain, sesuaikan domain sesuai kebutuhan
       // domain: 'yourdomain.com'
+      httpOnly: false
     },
   })
 );
