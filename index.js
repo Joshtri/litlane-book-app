a4aacf98f05670f8ddc09a32d239235c6322e614
Joshtri <stuffofyos1516@gmail.com>
2024-02-02 15:24:43 +0800

    lts

--- index.js
+++ index.js
@@ -1,99 +1,104 @@
-  require('dotenv').config();
-  const express = require('express');
-  const methodOverride = require('method-override');
-  const flash = require('connect-flash');
-  const session = require('express-session');
-  const nocache = require('nocache'); // Import nocache middleware
-  const connectDB = require('./config/db');
-  const { isLoggedIn } = require('./auth/protect');
-  const path = require('path');
+require('dotenv').config();
+const express = require('express');
+const methodOverride = require('method-override');
+const flash = require('connect-flash');
+const session = require('express-session');
+const nocache = require('nocache'); // Import nocache middleware
+const connectDB = require('./config/db');
+const { isLoggedIn } = require('./auth/protect');
+const path = require('path');
 
 
-  const app = express();
-  const PORT = process.env.PORT;
-  const customerRouter = require('./router/customer');
-  const bookRouter = require('./router/book');
-  const loginRouter = require('./router/login');
-  const dashboardRouter = require('./router/dashboard');
+const app = express();
+const PORT = process.env.PORT;
+const customerRouter = require('./router/customer');
+const bookRouter = require('./router/book');
+const loginRouter = require('./router/login');
+const dashboardRouter = require('./router/dashboard');
 
-  const userRouter = require('./router/mainUser');
+const userRouter = require('./router/mainUser');
 
-  connectDB();
-
-
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.json());
-  app.use(methodOverride('_method'));
+connectDB();
 
 
-  app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(methodOverride('_method'));
 
 
-  app.use('/uploads',express.static(path.join(__dirname,'uploads')));
-  // Express Session
-  app.use(
-      session({
-        secret: 'secret',
-        resave: true,
-        saveUninitialized: true,
-        cookie: {
-          maxAge : 360000,  // Waktu kadaluarsa dalam milidetik (6 menit)
-          // secure : true,
-        }
-      })
-  );
+app.use(express.static(__dirname + "/public"));
 
-  // app.use(
-  //   session({
-  //     secret: 'secret',
-  //     resave: true,
-  //     saveUninitialized: true,
-  //     cookie: {
-  //       maxAge: 360000,  // Waktu kadaluarsa dalam milidetik (6 menit)
-  //       secure: true,   // Hanya mengirim cookie melalui HTTPS
-  //       // sameSite: 'none', // Sesuaikan sesuai kebutuhan keamanan
-  //       // Jika Anda menggunakan subdomain, sesuaikan domain sesuai kebutuhan
-  //       // domain: 'yourdomain.com'
-  //       // httpOnly: false
-  //     },
-  //   })
-  // );
-  // Flash Messages
-  // Middleware checkAuth ditempatkan setelah express-session
-  // app.use(isLoggedIn);
-  app.use(flash({ sessionKeyName: 'flashMessage' }));
 
-  app.set('view engine', 'ejs');
+app.use('/uploads',express.static(path.join(__dirname,'uploads')));
 
-  // app.use('/', dashboardRouter);
 
-  // nocache middleware untuk mencegah caching halaman yang memerlukan otentikasi
-  app.use('/', loginRouter);
-  app.use('/', dashboardRouter ,isLoggedIn, nocache(), (req, res, next) => {
-    next();
-  });
-
-  app.use('/', customerRouter);
-  app.use('/', bookRouter);
-  app.use('/', userRouter)
+app.set('trust proxy', 1)
+// Express Session
+app.use(
+    session({
+      secret: 'secret',
+      resave: true,
+      saveUninitialized: true,
+      cookie: {
+        maxAge : 360000,  // Waktu kadaluarsa dalam milidetik (6 menit)
+        secure : true,
+      }
+    })
+);
 
 
 
+// app.use(
+//   session({
+//     secret: 'secret',
+//     resave: true,
+//     saveUninitialized: true,
+//     cookie: {
+//       maxAge: 360000,  // Waktu kadaluarsa dalam milidetik (6 menit)
+//       secure: true,   // Hanya mengirim cookie melalui HTTPS
+//       // sameSite: 'none', // Sesuaikan sesuai kebutuhan keamanan
+//       // Jika Anda menggunakan subdomain, sesuaikan domain sesuai kebutuhan
+//       // domain: 'yourdomain.com'
+//       // httpOnly: false
+//     },
+//   })
+// );
+// Flash Messages
+// Middleware checkAuth ditempatkan setelah express-session
+// app.use(isLoggedIn);
+app.use(flash({ sessionKeyName: 'flashMessage' }));
 
+app.set('view engine', 'ejs');
 
+// app.use('/', dashboardRouter);
 
+// nocache middleware untuk mencegah caching halaman yang memerlukan otentikasi
+app.use('/', loginRouter);
+app.use('/', dashboardRouter ,isLoggedIn, nocache(), (req, res, next) => {
+  next();
+});
 
-
-  app.set("views",[
-    path.join(__dirname, "/views"),
-    path.join(__dirname, "/views/customers"),
-    path.join(__dirname, "/views/books"),
-    path.join(__dirname, "/views/users"),
-  ])
+app.use('/', customerRouter);
+app.use('/', bookRouter);
+app.use('/', userRouter)
 
 
 
 
-  app.listen(PORT,()=>{
-      console.log(`server started at http://localhost:${PORT}`);
-  });
+
+
+
+
+app.set("views",[
+  path.join(__dirname, "/views"),
+  path.join(__dirname, "/views/customers"),
+  path.join(__dirname, "/views/books"),
+  path.join(__dirname, "/views/users"),
+])
+
+
+
+
+app.listen(PORT,()=>{
+    console.log(`server started at http://localhost:${PORT}`);
+});
