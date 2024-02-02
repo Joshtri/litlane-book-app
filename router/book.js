23e9275559da3967656f84eaea6d8b75b3858326
Joshtri <stuffofyos1516@gmail.com>
2024-02-03 07:10:38 +0800

    lts

--- router/book.js
+++ router/book.js
@@ -9,7 +9,7 @@ const {isLoggedIn} = require('../auth/protect');
 router.get('/data_book', isLoggedIn,bookPageView ); //get view
 // router.get('/data_book',isLoggedIn,bookPageView ); //get view
 router.get('/add_book', isLoggedIn,addBook); // get view
-router.post('/post_book', upload,postBook); // post book data
+router.post('/post_book', isLoggedIn,upload,postBook); // post book data
 router.get('/detail_book/:id',isLoggedIn, detailBook) //get view
 router.get('/edit_book/:id',isLoggedIn, editBook); // get view.
 
