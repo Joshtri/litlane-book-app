2f1458ff60f8f46873f3369bf1aff4a01e64c67a
Joshtri <stuffofyos1516@gmail.com>
2024-02-08 23:07:34 -0500

    test

--- router/login.js
+++ router/login.js
@@ -4,10 +4,6 @@ const router = express.Router();
 const {loginPage,createAdminAccount, postLoginUser} = require('../controllers/loginController');
 
 
-// Make sure process.env.API_BASE_URL_LOG is defined and not undefined
-if (!process.env.API_BASE_URL_LOG) {
-    throw new Error("API_BASE_URL_LOG environment variable is not defined.");
-}
 
 router.get(`/${process.env.API_BASE_URL_LOG}`, loginPage)
 
