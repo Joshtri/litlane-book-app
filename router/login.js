41f5666d1e82f4aaf5a70cfa5aee9c31f4fc96a7
Joshtri <stuffofyos1516@gmail.com>
2024-02-09 21:21:23 -0500

    fixing

--- router/login.js
+++ router/login.js
@@ -9,5 +9,5 @@ router.get(`/${process.env.API_BASE_URL_LOG}`, loginPage)
 
 router.post('/postCreate_Account', createAdminAccount)
 
-router.post('/postLogin_User', postLoginUser)
+router.post('/m33R34LZ4V65pH560836fQ/log/postLogin_User', postLoginUser)
 module.exports = router;
