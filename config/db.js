ee5641e3fcbad69a3b30460022d3cc99192117fc
Joshtri <stuffofyos1516@gmail.com>
2024-02-12 22:41:54 +0800

    lts

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGODB_URI, {
             // Tambahkan opsi timeout di sini (misalnya, 30 detik)
-            serverSelectionTimeoutMS: 100000,
+            serverSelectionTimeoutMS: 500000,
         });
 
         console.log(`Database connected. ${conn.connection.host}`);
