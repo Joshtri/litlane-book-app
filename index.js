382932df062e3e22c7857161ad21b903f3378a62
Joshtri <stuffofyos1516@gmail.com>
2024-02-12 22:00:55 +0800

    lts

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.use(
     name: 'LitlaneBook',
     store: new RedisStore({ 
       client: client,
-      ttl: 3600, // waktu kadaluwarsa dalam detik (misalnya 1 jam)
+      // ttl: 3600, // waktu kadaluwarsa dalam detik (misalnya 1 jam)
     
     
     }),
