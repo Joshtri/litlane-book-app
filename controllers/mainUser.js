228a9fe274ce8686d08ab74647eeab2f748c4cea
Joshtri <stuffofyos1516@gmail.com>
2024-02-27 21:08:30 +0800

    lts

--- controllers/mainUser.js
+++ controllers/mainUser.js
@@ -127,7 +127,7 @@ exports.mainBookPage = async (req, res) => {
         const startIndex = (currentPage - 1) * itemsPerPage;
 
         // Fetch books with pagination
-        // const booksPromise = Book.find().skip(startIndex).limit(itemsPerPage).lean().exec();
+        //// const booksPromise = Book.find().skip(startIndex).limit(itemsPerPage).lean().exec();
 
         // Fetch books with pagination
         const booksPromise = Book.find().sort({ createdAt: -1 }).skip(startIndex).limit(itemsPerPage).lean().exec();
